c321aaf52e39359fb167869a77781aa15f52f768
Jim O'Regan <joregan@kth.se>
2026-04-13 19:14:56 +0000

    remove generated files

--- src/app-wavesurfer/pkgIndex.tcl
+++
@@ -1,4 +0,0 @@
-# Automatically generated package index file
-# date   : Thu Jan 26 14:20:04 CET 2017
-# cmdline: build/mkPkgIndex.tcl src/app-wavesurfer/wavesurfer.tcl
-package ifneeded app-wavesurfer 1.8 [list source [file join $dir wavesurfer.tcl]]
